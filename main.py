27ac0b69063a93fd77674d8c8f629798c4c43c8b
Nikita <goloton037@gmail.com>
2024-09-25 22:54:58 +0300

    first commit

--- main.py
+++ main.py
@@ -1,30 +1,9 @@
-def fibonacci(n):
-    fib_sequence = [0, 1]
-    for i in range(2, n * (n + 1) // 2):  # Достаточно для создания пирамиды
-        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
-    return fib_sequence
+n = int(input("Введите количество школьников: "))
+k = int(input("Введите количество яблок: "))
 
-def print_fibonacci_pyramid(rows):
-    fib_sequence = fibonacci(rows)
-    index = 0
-    
-    for i in range(1, rows + 1):
-        # Печатаем пробелы для выравнивания
-        print(" " * (rows - i), end='')
-        for j in range(i):
-            print(fib_sequence[index], end=' ')
-            index += 1
-        print()  # Переход на новую строку
+yabloki = k // n
+ostatok = k - yabloki*n
 
-# Укажите количество строк пирамиды
-rows = 7
-print_fibonacci_pyramid(rows)
-
-def factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * factorial(n-1)
-
-# Добавьте вызов функции в конец файла
-print(f"Факториал числа {rows}: {factorial(rows)}")
+print(yabloki)
+print("По ", yabloki, "каждому школьнику.")
+print(ostatok, "яблок останется в корзине.")
